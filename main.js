2b5a54831672d72e2d230736e6b727907dd93d2a
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-07-14 09:18:19 +0200

    Addition Revlear JavaScript Animation in Sections

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ function hundlMenu() {
   menu.classList.toggle("active");
   actions.classList.toggle("active");
 }
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("Active");
+    } else {
+      reveals[i].classList.remove("Active");
+    }
+  }
+}
